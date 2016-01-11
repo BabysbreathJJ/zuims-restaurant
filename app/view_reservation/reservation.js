054d8ca1c118c837ca9fb7f2081b850d49ff943b
Lijingjing <lijingjing1008@gmail.com>
2016-01-11 17:12:14 +0800

    推送信息姓名显示bug fixed

--- app/view_reservation/reservation.js
+++ app/view_reservation/reservation.js
@@ -185,7 +185,7 @@ angular.module("myApp.reservation", ['ngRoute', 'smart-table', 'ui-notification'
 
         $scope.notify = function (order) {
             var newScope = $scope.$new(true);
-            newScope.name = order.userLastName + order.userFirstName;
+            newScope.name = order.userFirstName;
             newScope.phone = order.phoneId;
             newScope.level = order.userVipLevel;
             newScope.date = order.orderTime.split(" ")[0];
