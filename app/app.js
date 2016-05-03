a0709e9e58ef1e5dac456cee2359d6dd44c1d056
litongyu <342101826@qq.com>
2016-05-03 13:51:46 +0800

    rest api ip to domain name

--- app/app.js
+++ app/app.js
@@ -9,15 +9,23 @@ angular.module('myApp', [
         'myApp.chart',
         'myApp.basicInfo'
     ])
-    .constant('BaseUrl', 'http://115.159.87.129:')
+    .constant('BaseUrl', 'http://zui.ms/')
+    //.constant('BaseUrl', 'http://115.159.87.129:')
     //.constant('BaseUrl', 'http://202.120.40.175:')
-    .constant('userPort', '8008')
-    .constant('merchantPort', '8004')
+
+    .constant('userPort', 'api/operation')
+    //.constant('userPort', '8008')
+
+    .constant('merchantPort', 'api/merchant')
+    //.constant('merchantPort', '8004')
     //.constant('merchantPort', '21104')
-    .constant('managementPort', '8008')
+
+    .constant('managementPort', 'api/operation')
+    //.constant('managementPort', '8008')
     //.constant('managementPort', '21108')
-    .constant('restaurantPort', '8004')
-    //.constant('restaurantPort', '8000')
+
+    .constant('restaurantPort', 'api/merchant')
+    //.constant('restaurantPort', '8004')
     //.constant('restaurantPort', '21100')
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/management'});
