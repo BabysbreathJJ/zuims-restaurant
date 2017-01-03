0f74c0dfc880b9cb157ce3c45ae60a8c30ba8a17
syf1995 <853976413@qq.com>
2017-01-03 09:11:50 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -11,18 +11,18 @@ angular.module('myApp', [
         'myApp.productManagement'
     ])
 
-    //.constant('BaseUrl', 'https://api.bestfood.cc/')
-    //.constant('userPort', 'api/operation')
-    //.constant('merchantPort', 'api/merchant')
-    //.constant('managementPort', 'api/operation')
-    //.constant('restaurantPort', 'api/merchant')
+    .constant('BaseUrl', 'https://api.bestfood.cc/')
+    .constant('userPort', 'api/operation')
+    .constant('merchantPort', 'api/merchant')
+    .constant('managementPort', 'api/operation')
+    .constant('restaurantPort', 'api/merchant')
 
 
-    .constant('BaseUrl', 'http://115.159.87.129:')
-    .constant('userPort', '8008')
-    .constant('merchantPort', '8004')
-    .constant('managementPort', '8008')
-    .constant('restaurantPort', '8004')
+    //.constant('BaseUrl', 'http://115.159.87.129:')
+    //.constant('userPort', '8008')
+    //.constant('merchantPort', '8004')
+    //.constant('managementPort', '8008')
+    //.constant('restaurantPort', '8004')
 
     //.constant('BaseUrl', 'http://202.120.40.175:')
     //.constant('userPort', '8001')
