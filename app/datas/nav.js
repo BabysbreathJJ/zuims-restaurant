12e055f65ec523d69dd72b2a362de3959d6ee73e
syf1995 <853976413@qq.com>
2017-04-24 19:32:31 +0800

    ui

---
+++ app/datas/nav.js
@@ -0,0 +1,47 @@
+var navs = [
+{
+	"title": "订单",
+	"icon": "fa-stop-circle",
+	"href": "https://www.baidu.com",
+	"spread": false
+},{
+	"title": "餐厅",
+	"icon": "fa-stop-circle",
+	"href": "restaurant.html",
+	"spread": false
+},{
+	"title": "贩售",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "单点",
+		"icon": "&#xe641;",
+		"href": "button.html"
+	}, {
+		"title": "套餐",
+		"icon": "&#xe63c;",
+		"href": "form.html"
+	}, {
+		"title": "活动",
+		"icon": "&#xe63c;",
+		"href": "table.html"
+	}]
+}, {
+	"title": "统计",
+	"icon": "fa-cogs",
+	"spread": true,
+	"children": [{
+		"title": "本店",
+		"icon": "fa-table",
+		"href": "begtable.html"
+	}, {
+		"title": "全站",
+		"icon": "fa-navicon",
+		"href": "navbar.html"
+	}]
+}, {
+	"title": "最美食资讯站",
+	"icon": "fa-stop-circle",
+	"href": "http://zui.ms",
+	"spread": false
+}];
